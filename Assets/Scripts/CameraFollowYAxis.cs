7ee68a91f08389fc7c455d57abc61b1f31563b80
Bambosh <Sebastianmbsmith@gmail.com>
2022-04-29 03:33:11 +1000

    Added home environments

---
+++ Assets/Scripts/CameraFollowYAxis.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowYAxis : MonoBehaviour
+{
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = transform.position;
+        position.y = target.position.y;
+        transform.position = position;
+    }
+}
