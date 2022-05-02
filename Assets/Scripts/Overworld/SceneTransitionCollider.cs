251cabc18de102ae81db7f4af3ff8f9666d941a9
Jonathon Saxton (Bunker) <102579324@student.swin.edu.au>
2022-05-02 17:41:58 +1000

    File Restructure

---
+++ Assets/Scripts/Overworld/SceneTransitionCollider.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitionCollider : MonoBehaviour
+{
+    [SerializeField] private string nextScene;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+    }
+}
