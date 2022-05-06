913d08e8e67d778d02a3f67387769a2cc5356d48
RaymondBit <88523287+RaymondBit@users.noreply.github.com>
2022-05-06 12:16:30 +1000

    ChargedSpatula enemy added
    Some random script errors though

---
+++ Assets/Scripts/Minigames/ChargedSpatula/csEnemy.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csEnemy : MonoBehaviour
+{
+    private bool defeated = false;
+    private Vector3 direction;
+
+    void Start()
+    {
+        direction = new Vector3(Random.Range(1f, 5f), Random.Range(1f, 5f), 0);
+    }
+
+    void Update()
+    {
+        if (defeated == true)
+        {
+            Spin();
+        }
+    }
+
+    public void Spin()
+    {
+        transform.position += direction;
+        transform.Rotate(Vector3.back * 1000f * Time.deltaTime);
+    }
+
+    public void defeatedStateChange()
+    {
+        defeated = true;
+    }
+}
