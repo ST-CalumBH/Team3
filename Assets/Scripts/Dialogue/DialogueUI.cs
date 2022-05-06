e4683b04874cb7514dd896beee9846606f35bcdf
Christian22187 <80814210+Christian22187@users.noreply.github.com>
2022-05-06 10:50:45 +1000

    scripts are back

---
+++ Assets/Scripts/Dialogue/DialogueUI.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class DialogueUI : MonoBehaviour
+{
+    [SerializeField] private GameObject dialogueBox;
+    [SerializeField] private TMP_Text textLabel;
+
+    public bool IsOpen { get; private set; }
+
+    private TypewriterEffect typewriterEffect;
+
+    private void Start()
+    {
+        typewriterEffect = GetComponent<TypewriterEffect>();
+        CloseDialogueBox();
+    }
+
+    public void ShowDialogue(DialogueObject dialogueObject)
+    {
+        IsOpen = true;
+        dialogueBox.SetActive(true);
+        StartCoroutine(StepThroughDialogue(dialogueObject));
+    }
+
+    private IEnumerator StepThroughDialogue(DialogueObject dialogueObject)
+    {
+        foreach (string dialogue in dialogueObject.Dialogue)
+        {
+            yield return typewriterEffect.Run(dialogue, textLabel);
+            yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+        }
+
+        CloseDialogueBox();
+    }
+
+
+    private void CloseDialogueBox()
+    {
+        IsOpen = false;
+        dialogueBox.SetActive(false);
+        textLabel.text = string.Empty;
+    }
+}
