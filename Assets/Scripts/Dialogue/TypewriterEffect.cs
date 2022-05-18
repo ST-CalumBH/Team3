34dc7a3e981fcd3d9ecb6ac023be21a61b82008b
Christian22187 <Christiankiel@outlook.com>
2022-05-18 23:17:25 +1000

    We got punctuation and skipping stuff now

--- Assets/Scripts/Dialogue/TypewriterEffect.cs
+++ Assets/Scripts/Dialogue/TypewriterEffect.cs
@@ -7,6 +7,8 @@ public class TypewriterEffect : MonoBehaviour
 {
     [SerializeField] private float typewriterSpeed = 50f;
 
+    public bool IsRunning { get; private set; }
+
     private readonly Dictionary<HashSet<char>, float> punctuations = new Dictionary<HashSet<char>, float>()
     {
         {new HashSet<char>(){'.', '!', '?'}, 0.6f},
@@ -14,13 +16,22 @@ public class TypewriterEffect : MonoBehaviour
     };
 
 
-    public Coroutine Run(string textToType, TMP_Text textLabel)
+    private Coroutine typingCoroutine;
+
+    public void Run(string textToType, TMP_Text textLabel)
     {
-        return StartCoroutine(TypeText(textToType, textLabel));
+        typingCoroutine = StartCoroutine(TypeText(textToType, textLabel));
+    }
+
+    public void Stop()
+    {
+        StopCoroutine(typingCoroutine);
+        IsRunning = false;
     }
 
     private IEnumerator TypeText(string textToType, TMP_Text textLabel)
     {
+        IsRunning = true;
         textLabel.text = string.Empty;
 
         float t = 0;
@@ -50,7 +61,7 @@ public class TypewriterEffect : MonoBehaviour
             yield return null;
         }
 
-        textLabel.text = textToType;
+        IsRunning = false;
     }
 
     private bool IsPunctuation(char character, out float waitTime)
