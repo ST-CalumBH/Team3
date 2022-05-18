d358a8d38e82be98463b1a8daec081787d903430
Christian22187 <Christiankiel@outlook.com>
2022-05-18 19:56:03 +1000

    punctuation

--- Assets/Scripts/Dialogue/TypewriterEffect.cs
+++ Assets/Scripts/Dialogue/TypewriterEffect.cs
@@ -7,6 +7,13 @@ public class TypewriterEffect : MonoBehaviour
 {
     [SerializeField] private float typewriterSpeed = 50f;
 
+    private readonly Dictionary<HashSet<char>, float> punctuations = new Dictionary<HashSet<char>, float>()
+    {
+        {new HashSet<char>(){'.', '!', '?'}, 0.6f},
+        {new HashSet<char>(){',', ';', ':'}, 0.3f},
+    };
+
+
     public Coroutine Run(string textToType, TMP_Text textLabel)
     {
         return StartCoroutine(TypeText(textToType, textLabel));
@@ -21,15 +28,46 @@ public class TypewriterEffect : MonoBehaviour
 
         while (charIndex < textToType.Length)
         {
+            int lastCharIndex = charIndex;
+
+
             t += Time.deltaTime * typewriterSpeed;
             charIndex = Mathf.FloorToInt(t);
             charIndex = Mathf.Clamp(charIndex, 0, textToType.Length);
 
-            textLabel.text = textToType.Substring(0, charIndex);
+            for (int i = lastCharIndex; i < charIndex; i++)
+            {
+                bool isLast = i >= textToType.Length - 1;
+
+                textLabel.text = textToType.Substring(0, i + 1);
+
+                if (IsPunctuation(textToType[i], out float waitTime) && !isLast && !IsPunctuation(textToType[i + 1], out _))
+                {
+                    yield return new WaitForSeconds(waitTime);
+                }
+            }
 
             yield return null;
         }
 
         textLabel.text = textToType;
+    }
+
+    private bool IsPunctuation(char character, out float waitTime)
+    {
+        foreach(KeyValuePair<HashSet<char>, float> punctuationCategory in punctuations)
+        {
+            if (punctuationCategory.Key.Contains(character))
+            {
+                waitTime = punctuationCategory.Value;
+            
+
+        
+                return true;
+            }
+        }
+
+        waitTime = default;
+        return false;
     }
 }
