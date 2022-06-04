a88a0482fd3b047ca7f65e3fe4b219f98ae34cf2
BProjectx <hoangthong2609@gmail.com>
2022-06-05 06:00:06 +1000

    Adding Animation to most of the Scene
    - Add fade in/out animation to mulitple scene
    - Integrate the animation into the SceneTransitionCollider Scripts

--- Assets/LevelLoader.cs
+++ Assets/LevelLoader.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class LevelLoader : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/Dialogue/SceneLoadDialogue.cs
+++ Assets/Scripts/Dialogue/SceneLoadDialogue.cs
@@ -5,8 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoadDialogue : MonoBehaviour
 {
-    [SerializeField] DialogueUI DialogueBox; //Canvas with DialogueUI script attached 
-    [SerializeField] DialogueObject DialogueObject; //Dialogue object to be played
+    [SerializeField] DialogueUI dialogueBox; //Canvas with DialogueUI script attached 
+    [SerializeField] DialogueObject dialogueObject; //Dialogue object to be played
     bool isActive = false;
 
     // Start is called before the first frame update
@@ -20,9 +20,9 @@ public class SceneLoadDialogue : MonoBehaviour
     
     IEnumerator PlayDialogue()
     {
-        DialogueBox.ShowDialogue(DialogueObject); //the line that plays the script from dialogue object
+        dialogueBox.ShowDialogue(dialogueObject); //the line that plays the script from dialogue object
         isActive = true;
-        yield return new WaitUntil(() => DialogueBox.IsOpen == false);
+        yield return new WaitUntil(() => dialogueBox.IsOpen == false);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
