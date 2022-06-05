9b1c3fc899b23e8b6e14018d457fc79c11ec1bf5
BProjectx <hoangthong2609@gmail.com>
2022-06-05 19:46:37 +1000

    Add a Tutorial Overlay
    - A Tutorial Overlay freezes the Sheep Jumping minigame
    - Unfreeze when press space (acknowledge how to play)
    - Overlay disappears and game starts
    - Add some sprites as tutorial
    
    - Edit some PauseMenu in some scenes, they were using the wrong one

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -13,6 +13,8 @@ public class PauseMenu : MonoBehaviour
     [SerializeField] GameObject pauseMenuUI;
     [SerializeField] playerController player;
 
+
+    // Start is called before the first frame update
     void Start()
     {
         pauseMenuUI.SetActive(false);
