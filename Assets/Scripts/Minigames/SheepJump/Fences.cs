6169c302ddad12f38e139af12272400ebcf1e1ce
ST-CalumBH <101605642@student.swin.edu.au>
2022-06-05 17:28:42 +1000

    Added audio to sheep jump and fixed it up post merge

--- Assets/Scripts/Minigames/SheepJump/Fences.cs
+++ Assets/Scripts/Minigames/SheepJump/Fences.cs
@@ -30,7 +30,7 @@ public class Fences : MonoBehaviour
 
         pos.x -= player.velocity.x * Time.fixedDeltaTime;
 
-        if (pos.x <= -18)
+        if (pos.x <= -12)
             pos.x = 49;
 
         transform.position = pos;
