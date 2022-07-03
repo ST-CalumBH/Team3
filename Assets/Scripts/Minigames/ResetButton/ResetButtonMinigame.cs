35e5f31a5122d71fe4f81f72d75703706e44ef7b
Jonathon S <102579324@student.swin.edu.au>
2022-07-04 02:00:45 +1000

    Finished Functionality

--- Assets/Scripts/Minigames/ResetButton/ResetButtonMinigame.cs
+++ Assets/Scripts/Minigames/ResetButton/ResetButtonMinigame.cs
@@ -9,27 +9,87 @@ public class ResetButtonMinigame : Minigame
     public GameObject Button;
     public Slider timebar;
     public Slider selector;
+    public Transform handTransform;
+    public GameObject selectorPanel;
 
-    BoxCollider2D handCollider;
-    BoxCollider2D buttonCollider;
+    public float bounceAngle = 45f;//degrees the hand varies from x axis either side
+    public float zRotate = 1f;//Speed of movement
+    public float hitZone = 0.2f;//decimal percentage of bar that is a hit
+    public float maxTime = 15f;//max time measured in seconds
 
-    Transform handTransform;
-    float zRotate = 0;
+    enum State {CLOCKWISE, COUNTERCLOCKWISE}
+
+    State curState;
+    float time = 0f;//current time variable, measured in seconds
+    float zoneDegrees;
+
     // Start is called before the first frame update
     void Start()
     {
         timebar.value = timebar.maxValue;
         selector.value = (selector.maxValue+selector.minValue)/2;
-        handCollider = Hand.GetComponent<BoxCollider2D>();
-        buttonCollider = Button.GetComponent<BoxCollider2D>();
-        handTransform = Hand.transform;
-        
+        curState = State.CLOCKWISE;
+        RectTransform sp = selectorPanel.GetComponent<RectTransform>();
+        sp.sizeDelta = new Vector2(1500 * hitZone, 70);
+        zoneDegrees = 90 * hitZone / 2;
+        timebar.maxValue = maxTime;
+        timebar.value = 0f;
+        StartCoroutine(Timer());
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        handTransform.rotation.Set(handTransform.rotation.x, handTransform.rotation.y, zRotate, handTransform.rotation.w);
-        zRotate += 0.1f;
+
+        if (curState == State.COUNTERCLOCKWISE)
+        {
+            handTransform.Rotate(handTransform.rotation.x, handTransform.rotation.y, zRotate);
+            selector.value += zRotate;
+        }
+        if ( curState == State.CLOCKWISE)
+        {
+            handTransform.Rotate(handTransform.rotation.x, handTransform.rotation.y, -zRotate);
+            selector.value -= zRotate;
+        }
+        if (handTransform.eulerAngles.z > bounceAngle)
+        {
+            if (handTransform.eulerAngles.z < (360f - bounceAngle) - 2f)
+            {
+                curState = State.CLOCKWISE;
+            }
+        }
+        if (handTransform.eulerAngles.z < (360f - bounceAngle))
+        {
+            if (handTransform.eulerAngles.z > bounceAngle + 2f)
+            {
+                curState = State.COUNTERCLOCKWISE;
+            }
+        }
     }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (handTransform.eulerAngles.z > (360-zoneDegrees) || handTransform.eulerAngles.z < zoneDegrees)
+            {
+                StartCoroutine(EndMinigame(true));
+            }
+        }
+        if (time >= maxTime)
+        {
+            StartCoroutine(EndMinigame(false));
+        }
+    }
+
+    IEnumerator Timer()
+    {
+        time += 0.01f;
+        timebar.value = time;
+        yield return new WaitForSeconds(0.01f);
+        if (time <= maxTime)
+        {
+            StartCoroutine(Timer());
+        }
+    }
+
 }
