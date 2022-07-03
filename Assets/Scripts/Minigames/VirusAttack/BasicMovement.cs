5662b2dff9c5d3a9958bddd721df3e4ff366dffd
RaymondBit <88523287+RaymondBit@users.noreply.github.com>
2022-07-04 02:03:59 +1000

    scrolling and movement set up

---
+++ Assets/Scripts/Minigames/VirusAttack/BasicMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    private Vector2 movement;
+
+
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
