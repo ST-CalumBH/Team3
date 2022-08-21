71ed18adfc78b73518f6e546365e29ae6f627640
RaymondBit <88523287+RaymondBit@users.noreply.github.com>
2022-08-22 00:37:30 +1000

    CameraShake in sword dodge minigame implemented

--- Assets/Scripts/Minigames/SwordDodge/CameraShake.cs
+++ Assets/Scripts/Minigames/SwordDodge/CameraShake.cs
@@ -4,21 +4,40 @@ using UnityEngine;
 
 public class CameraShake : MonoBehaviour
 {
-    public Transform cameraTransform = default;
-    private Vector3 _originalPosOfCam = default;
-    public float shakeFrequency = default;
+    public Camera mainCam;
+    public float shakeFrequency;
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        _originalPosOfCam = cameraTransform.position;
-        cameraTransform = Transform.FindObjectOfType<Camera>().transform;
+        if (mainCam == null) mainCam = Camera.main;
+    }
+
+    public void Shake()
+    {
+        shakeFrequency = 0.1f;
+        InvokeRepeating("CameraBake", 0, 0.01f);
+        Invoke("CameraStop", 0.2f);
     }
 
     private void CameraBake()
     {
-        cameraTransform.position = _originalPosOfCam + Random.insideUnitSphere * shakeFrequency;
+        if (shakeFrequency > 0)
+        {
+            Vector3 camPos = mainCam.transform.position;
+
+            float offsetX = Random.value * shakeFrequency * 2 - shakeFrequency;
+            float offsetY = Random.value * shakeFrequency * 2 - shakeFrequency;
+            camPos.x += offsetX;
+            camPos.y += offsetY;
+
+            mainCam.transform.position = camPos;
+        }
     }
-        
-  
+
+    private void CameraStop()
+    {
+        CancelInvoke("CameraBake");
+        mainCam.transform.localPosition = new Vector3(0,0,-10);
+
+    }
 }
