a9527b627bb04fd3ca6845ce2258cc1168206ad4
ST-CalumBH <101605642@student.swin.edu.au>
2022-08-23 22:05:18 +1000

    Updating

--- Assets/Scripts/Minigames/PottyRacers/CarController.cs
+++ Assets/Scripts/Minigames/PottyRacers/CarController.cs
@@ -8,6 +8,7 @@ public class CarController : MonoBehaviour
     [Header("Car Settings")]
 
     public float driftFactor = 0.95f;
+    public float maxSpeed = 20;
 
     public float accelFactor = 30.0f;
     public float turnFacotor = 3.5f;
@@ -17,6 +18,7 @@ public class CarController : MonoBehaviour
     float steeringInput = 0;
 
     float rotationAngle = 0;
+    float velocityVsUp = 0;
 
     //components
     Rigidbody2D carRigidbody2D;
@@ -41,14 +43,44 @@ public class CarController : MonoBehaviour
     void ApplyEngineForce()
 
     {
+
+        velocityVsUp = Vector2.Dot(transform.up, carRigidbody2D.velocity);
+
+        //limit the forward speed
+        if (velocityVsUp > maxSpeed && accelInput > 0)
+            return;
+
+        //limit reverse speed
+        if (velocityVsUp < -maxSpeed * 0.5f && accelInput < 0)
+            return;
+
+        //limit side speed
+        if (carRigidbody2D.velocity.sqrMagnitude > maxSpeed  * maxSpeed && accelInput > 0)
+            return;
+
+
+        //applying drag or friction when zero accel
+
+        if (accelInput == 0)
+            carRigidbody2D.drag = Mathf.Lerp(carRigidbody2D.drag, 3.0f, Time.fixedDeltaTime * 3);
+        else carRigidbody2D.drag = 0;
+
         Vector2 engineForceVector = transform.up * accelInput * accelFactor;
 
         carRigidbody2D.AddForce(engineForceVector, ForceMode2D.Force);
+
+
     }
 
     void ApplySteering()
     {
-        rotationAngle -= steeringInput * turnFacotor;
+
+        //limit car turning when no forward momentum is being applied
+        // value of 8 can be changed
+        float minSpeedBeforeTurn = (carRigidbody2D.velocity.magnitude / 8);
+        minSpeedBeforeTurn = Mathf.Clamp01(minSpeedBeforeTurn);
+
+        rotationAngle -= steeringInput * turnFacotor * minSpeedBeforeTurn;
 
         carRigidbody2D.MoveRotation(rotationAngle);
     }
