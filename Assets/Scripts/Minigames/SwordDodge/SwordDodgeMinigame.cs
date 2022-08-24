2d844d94a6580ebd89c8325edc2686c5b27af50f
Jonathon S <102579324@student.swin.edu.au>
2022-08-24 23:19:21 +1000

    (Sword Dodge Minigame) Fixed issue where upon getting hit a dodge would still register

--- Assets/Scripts/Minigames/SwordDodge/SwordDodgeMinigame.cs
+++ Assets/Scripts/Minigames/SwordDodge/SwordDodgeMinigame.cs
@@ -6,203 +6,222 @@ using Combat;
 
 namespace SwordDodge {
     public class SwordDodgeMinigame : Minigame
-{
-    [SerializeField] private AudioClip clang;
-
-    AudioSource audioSource;
-
-    public GameObject animatorGO;
-    Animator animator;
-
-    public Warning lWarningGO;
-    public Warning mWarningGO;
-    public Warning rWarningGO;
-
-    public SwordDodgePlayer player;
-
-    public List<Warning> warnings;
-
-    public Text text;
-
-    public ParticleSystem hurt;
-
-    SpriteRenderer lWarning;
-    SpriteRenderer mWarning;
-    SpriteRenderer rWarning;
-
-    int laneNum;
-    public int dodgeCount = 0;
-    public bool isAttacking;
-    bool cooldown = false;
-
-    // Start is called before the first frame update
-    void Start()
     {
-        audioSource = GetComponent<AudioSource>();
-        animator = animatorGO.GetComponent<Animator>();
-        animator.StopPlayback();
-        warnings.Add(lWarningGO);
-        warnings.Add(mWarningGO);
-        warnings.Add(rWarningGO);
-        lWarning = lWarningGO.GetComponent<SpriteRenderer>();
-        mWarning = mWarningGO.GetComponent<SpriteRenderer>();
-        rWarning = rWarningGO.GetComponent<SpriteRenderer>();
-        lWarning.enabled = false;
-        mWarning.enabled = false;
-        rWarning.enabled = false;
-        player = FindObjectOfType<SwordDodgePlayer>();
-        laneNum = player.curPosition;
-        StartCoroutine(ShowWarning());
-    }
+        [SerializeField] private AudioClip clang;
 
-    // Update is called once per frame
-    void Update()
-    {
-        text.text = dodgeCount.ToString();
-        if (!cooldown)
+        AudioSource audioSource;
+
+        public GameObject animatorGO;
+        Animator animator;
+
+        public Warning lWarningGO;
+        public Warning mWarningGO;
+        public Warning rWarningGO;
+
+        public SwordDodgePlayer player;
+
+        public List<Warning> warnings;
+
+        public Text text;
+
+        public ParticleSystem hurt;
+
+        SpriteRenderer lWarning;
+        SpriteRenderer mWarning;
+        SpriteRenderer rWarning;
+
+        int laneNum;
+        public int dodgeCount = 0;
+        public bool isAttacking;
+        bool cooldown = false;
+
+        // Start is called before the first frame update
+        void Start()
         {
-            if (isAttacking == true)
+            audioSource = GetComponent<AudioSource>();
+            animator = animatorGO.GetComponent<Animator>();
+            animator.StopPlayback();
+            warnings.Add(lWarningGO);
+            warnings.Add(mWarningGO);
+            warnings.Add(rWarningGO);
+            lWarning = lWarningGO.GetComponent<SpriteRenderer>();
+            mWarning = mWarningGO.GetComponent<SpriteRenderer>();
+            rWarning = rWarningGO.GetComponent<SpriteRenderer>();
+            lWarning.enabled = false;
+            mWarning.enabled = false;
+            rWarning.enabled = false;
+            player = FindObjectOfType<SwordDodgePlayer>();
+            laneNum = player.curPosition;
+            StartCoroutine(ShowWarning());
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            text.text = dodgeCount.ToString();
+            if (!cooldown)
             {
-                switch (laneNum)
+                if (isAttacking == true)
                 {
-                    case 0:
-                        if (player.isTouching == PositionEnum.LEFT)
-                        {
-                            Debug.Log("Left Hit");
-                            dodgeCount = 0;
-                            StartCoroutine(Hurt());
-                            StartCoroutine(CooldownTimer());
-                        }
-                        break;
-                    case 1:
-                        if (player.isTouching == PositionEnum.MIDDLE)
-                        {
-                            Debug.Log("Middle Hit");
-                            dodgeCount = 0;
-                            StartCoroutine(Hurt());
-                            StartCoroutine(CooldownTimer());
-                        }
-                        break;
-                    case 2:
-                        if (player.isTouching == PositionEnum.RIGHT)
-                        {
-                            Debug.Log("Right Hit");
-                            dodgeCount = 0;
-                            StartCoroutine(Hurt());
-                            StartCoroutine(CooldownTimer());
-                        }
-                        break;
+                    switch (laneNum)
+                    {
+                        case 0:
+                            if (player.isTouching == PositionEnum.LEFT)
+                            {
+                                //Debug.Log("Left Hit");
+                                Debug.Log("Reset");
+                                dodgeCount = 0;
+                                StartCoroutine(Hurt());
+                                StartCoroutine(CooldownTimer());
+                            }
+                            else
+                            {
+                                dodgeCount++;
+                                StartCoroutine(CooldownTimer());
+                            }
+                            break;
+                        case 1:
+                            if (player.isTouching == PositionEnum.MIDDLE)
+                            {
+                                //Debug.Log("Middle Hit");
+                                Debug.Log("Reset");
+                                dodgeCount = 0;
+                                StartCoroutine(Hurt());
+                                StartCoroutine(CooldownTimer());
+                            }
+                            else
+                            {
+                                dodgeCount++;
+                                StartCoroutine(CooldownTimer());
+                            }
+                            break;
+                        case 2:
+                            if (player.isTouching == PositionEnum.RIGHT)
+                            {
+                                //Debug.Log("Right Hit");
+                                Debug.Log("Reset");
+                                dodgeCount = 0;
+                                StartCoroutine(Hurt());
+                                StartCoroutine(CooldownTimer());
+                            }
+                            else
+                            {
+                                dodgeCount++;
+                                StartCoroutine(CooldownTimer());
+                            }
+                            break;
+                    }
                 }
             }
         }
-    }
 
-    public void Slash()
-    {
-        switch (laneNum)
+        public void Slash()
         {
-            case 0:
-                StartCoroutine(LeftAttack());
-            break;
-            case 1:
-                StartCoroutine(MiddleAttack());
-            break;
-            case 2:
-                StartCoroutine(RightAttack());
-            break;
-            default:
-                StartCoroutine(MiddleAttack());
-            break;
-        };
+            switch (laneNum)
+            {
+                case 0:
+                    StartCoroutine(LeftAttack());
+                break;
+                case 1:
+                    StartCoroutine(MiddleAttack());
+                break;
+                case 2:
+                    StartCoroutine(RightAttack());
+                break;
+                default:
+                    StartCoroutine(MiddleAttack());
+                break;
+            };
         
-    }
-
-    public void CallShowWarning()
-    {
-        StartCoroutine(ShowWarning());
-    }
-
-    public void CallEndMinigame()
-    {
-        StartCoroutine(EndMinigame());
-    }
-
-    public void CheckForTnight()//I love Fortnite
-    {
-        if (dodgeCount == 3)
-        {
-            StartCoroutine(EndMinigame());
         }
-        else
+
+        public void CallShowWarning()
         {
             StartCoroutine(ShowWarning());
         }
-    }
 
-    IEnumerator ShowWarning()
-    {
-        yield return new WaitForSeconds(1f);
-        warnings[laneNum].StartFlash();
-        yield return new WaitForSeconds(warnings[laneNum].GetTotalFlashTime() + 1f);
-        Slash();
-    }
+        public void CallEndMinigame()
+        {
+            StartCoroutine(EndMinigame());
+        }
 
-    IEnumerator LeftAttack()
-    {
-        animator.Play("Base Layer.Left Lane");
-        yield return new WaitForSeconds(0.30f); //Currently just manually inputting the animation length, but should make a dynamic system.
-        isAttacking = true;
-        audioSource.PlayOneShot(clang);
-        yield return new WaitForSeconds(0.74f);
-        isAttacking = false;
-        yield return new WaitForSeconds(0.05f);
-        animator.Play("Base Layer.Neutral");
-        laneNum = player.curPosition;
-        dodgeCount++;
-        CheckForTnight();
-    }
-    IEnumerator MiddleAttack()
-    {
-        animator.Play("Base Layer.Mid Lane");
-        yield return new WaitForSeconds(0.30f);
-        isAttacking = true;
-        audioSource.PlayOneShot(clang);
-        yield return new WaitForSeconds(0.74f);
-        isAttacking = false;
-        yield return new WaitForSeconds(0.05f);
-        animator.Play("Base Layer.Neutral");
-        laneNum = player.curPosition;
-        dodgeCount++;
-        CheckForTnight();
-    }
-    IEnumerator RightAttack()
-    {
-        animator.Play("Base Layer.Right Lane");
-        yield return new WaitForSeconds(0.30f);
-        isAttacking = true;
-        audioSource.PlayOneShot(clang);
-        yield return new WaitForSeconds(0.74f);
-        isAttacking = false;
-        yield return new WaitForSeconds(0.05f);
-        animator.Play("Base Layer.Neutral");
-        laneNum = player.curPosition;
-        dodgeCount++;
-        CheckForTnight();
-    }
+        public void CheckForTnight()//I love Fortnite
+        {
+            if (dodgeCount == 3)
+            {
+                StartCoroutine(EndMinigame());
+            }
+            else
+            {
+                StartCoroutine(ShowWarning());
+            }
+        }
 
-    IEnumerator Hurt()
-    {
-        ParticleSystem ps = Instantiate(hurt, player.transform);
-        ps.Play();
-        yield return new WaitForSeconds(2f);
-        Destroy(ps);
-    }
+        IEnumerator ShowWarning()
+        {
+            yield return new WaitForSeconds(1f);
+            warnings[laneNum].StartFlash();
+            yield return new WaitForSeconds(warnings[laneNum].GetTotalFlashTime() + 1f);
+            Slash();
+        }
 
-    IEnumerator CooldownTimer()
-    {
-        cooldown = true;
-        yield return new WaitForSeconds(1.5f);
-        cooldown = false;
+        IEnumerator LeftAttack()
+        {
+            animator.Play("Base Layer.Left Lane");
+            yield return new WaitForSeconds(0.30f); //Currently just manually inputting the animation length, but should make a dynamic system.
+            isAttacking = true;
+            audioSource.PlayOneShot(clang);
+            yield return new WaitForSeconds(0.74f);
+            isAttacking = false;
+            yield return new WaitForSeconds(0.05f);
+            animator.Play("Base Layer.Neutral");
+            laneNum = player.curPosition;
+            Debug.Log("Increment");
+            CheckForTnight();
+        }
+        IEnumerator MiddleAttack()
+        {
+            animator.Play("Base Layer.Mid Lane");
+            yield return new WaitForSeconds(0.30f);
+            isAttacking = true;
+            audioSource.PlayOneShot(clang);
+            yield return new WaitForSeconds(0.74f);
+            isAttacking = false;
+            yield return new WaitForSeconds(0.05f);
+            animator.Play("Base Layer.Neutral");
+            laneNum = player.curPosition;
+            Debug.Log("Increment");
+            
+            CheckForTnight();
+        }
+        IEnumerator RightAttack()
+        {
+            animator.Play("Base Layer.Right Lane");
+            yield return new WaitForSeconds(0.30f);
+            isAttacking = true;
+            audioSource.PlayOneShot(clang);
+            yield return new WaitForSeconds(0.74f);
+            isAttacking = false;
+            yield return new WaitForSeconds(0.05f);
+            animator.Play("Base Layer.Neutral");
+            laneNum = player.curPosition;
+            Debug.Log("Increment");
+            CheckForTnight();
+        }
+
+        IEnumerator Hurt()
+        {
+            ParticleSystem ps = Instantiate(hurt, player.transform);
+            ps.Play();
+            yield return new WaitForSeconds(2f);
+            Destroy(ps);
+        }
+
+        IEnumerator CooldownTimer()
+        {
+            cooldown = true;
+            yield return new WaitForSeconds(1.5f);
+            cooldown = false;
+        }
     }
-}
 }
