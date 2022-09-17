3b8328c01dcbf440f5707e103d7d6843f9b84cda
RaymondBit <88523287+RaymondBit@users.noreply.github.com>
2022-09-18 00:03:02 +1000

    eventTimeline script now capable of playing events onTriggerEnter and onInteraction, onAwake is buggy atm

--- Assets/Scripts/Overworld/eventIcon.cs
+++ Assets/Scripts/Overworld/eventIcon.cs
@@ -5,63 +5,11 @@ using UnityEngine;
 namespace Overworld {
     public class eventIcon : MonoBehaviour
     {
-
-        private bool active; // 0 = floating anim, 1 = hide
-
-        private bool floatUp;
-        private int floatCounter;
-
-        private Vector3 vectorUp = (Vector3.up / 100);
-        private Vector3 vectorDown = (Vector3.down / 100);
-
         public Animator animator;
 
-        void Start()
+        public void changeActiveState(bool state)
         {
-            active = true;
-            floatUp = true;
-            floatCounter = 0;
-        }
-
-        
-        void FixedUpdate()
-        {
-            if (active)
-            {
-                floatingAnimation();
-            }
-        }
-
-        public void floatingAnimation()
-        {
-            if (floatUp)
-            {
-                transform.position += vectorUp;
-                floatCounter++;
-
-                if (floatCounter == 100)
-                {
-                    floatUp = false;
-                    floatCounter = 0;
-                }
-            }
-            else
-            {
-                transform.position += vectorDown;
-                floatCounter++;
-
-                if (floatCounter == 100)
-                {
-                    floatUp = true;
-                    floatCounter = 0;
-                }
-            }
-        }
-
-        public void changeActiveState()
-        {
-            active = !active;
-            gameObject.SetActive(active);
+            gameObject.SetActive(state);
         }
 
         void OnTriggerEnter2D(Collider2D collision)
