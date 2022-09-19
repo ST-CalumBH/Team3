962dcd63a977248622f6dfc93a0cd9b28b7a5d52
ST-CalumBH <101605642@student.swin.edu.au>
2022-09-19 16:26:35 +1000

    fixed code

--- Assets/Scripts/Minigames/PottyRacers/CarLapCounter.cs
+++ Assets/Scripts/Minigames/PottyRacers/CarLapCounter.cs
@@ -2,20 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.UI;
 
 public class CarLapCounter : MonoBehaviour
 {
+
+    public Text carPosText;
+
     int passedCPNo = 0;
     float timeAtLastPassedCP = 0;
 
     int numberOfPastCheckpoints = 0;
 
+    int lapsCompleted = 0;
+    const int lapsToComplete = 2;
+
+    bool isRaceFinished = false;
+
+    int carPos = 0;
+
+
     public event Action<CarLapCounter> OnPassCheckpoint;
+
+    public void SetCarPostion(int position)
+    {
+        carPos = position;
+
+    }
+
+    public int GetNumberofCheckPointsPassed()
+    {
+        return passedCPNo;
+    }
+
+    public float GetTimeAtLastCheckPoint()
+    {
+        return timeAtLastPassedCP;
+    }
+
+    IEnumerator ShowPositionCO(float delayUntilHidePosition)
+    {
+        carPosText.text = carPos.ToString();
+
+        carPosText.gameObject.SetActive(true);
+
+        yield return new WaitForSeconds(delayUntilHidePosition);
+
+        carPosText.gameObject.SetActive(false);
+    }
 
     private void OnTriggerEnter2D(Collider2D collider2D)
     {
         if(collider2D.CompareTag("Checkpoint"))
         {
+            if (isRaceFinished)
+                return;
+
             Checkpoint checkpoint = collider2D.GetComponent<Checkpoint>();
             // car is going through correct checkpoints
         if (passedCPNo + 1 == checkpoint.CheckPointNo)
@@ -25,9 +67,22 @@ public class CarLapCounter : MonoBehaviour
                 numberOfPastCheckpoints++;
 
                 timeAtLastPassedCP = Time.time;
+                if (checkpoint.isFinishLine)
+                {
+                    passedCPNo = 0;
+                    lapsCompleted++;
+
+                    if(lapsCompleted >= lapsToComplete)
+                    
+                        isRaceFinished = true;
+                }
 
                 //invoke event
                 OnPassCheckpoint?.Invoke(this);
+
+                if (isRaceFinished)
+                    StartCoroutine(ShowPositionCO(100));
+                else StartCoroutine(ShowPositionCO(1.5f));
             }
         }
     }
--- Assets/Scripts/Minigames/PottyRacers/PositionHandler.cs
+++ Assets/Scripts/Minigames/PottyRacers/PositionHandler.cs
@@ -1,12 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+
 
 public class PositionHandler : MonoBehaviour
 {
-    public List<CarLapCounter>
-    private void Start()
+    public List<CarLapCounter> carLapCounters = new List<CarLapCounter>();
+
+    void Start()
     {
-        
+        CarLapCounter[] carLapCounterArray = FindObjectsOfType<CarLapCounter>();
+
+        carLapCounters = carLapCounterArray.ToList<CarLapCounter>();
+
+        foreach (CarLapCounter lapCounter in carLapCounters)
+            lapCounter.OnPassCheckpoint += OnPassCheckpoint;
+
+    }
+
+    void OnPassCheckpoint(CarLapCounter carLapCounter)
+    {
+        //display car position number
+        carLapCounters = carLapCounters.OrderByDescending(s => s.GetNumberofCheckPointsPassed()).ThenBy(s => s.GetTimeAtLastCheckPoint()).ToList();
+
+        int carPos = carLapCounters.IndexOf(carLapCounter) + 1;
+
+        carLapCounter.SetCarPostion(carPos);
     }
 }
