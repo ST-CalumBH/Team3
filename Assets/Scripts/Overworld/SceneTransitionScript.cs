d9dcb5d3d2449cac7c47ae1a0d012eed1c82916d
RaymondBit <88523287+RaymondBit@users.noreply.github.com>
2022-09-29 00:42:36 +1000

    scene transition changes, re-added cursor.unlock

--- Assets/Scripts/Overworld/SceneTransitionScript.cs
+++ Assets/Scripts/Overworld/SceneTransitionScript.cs
@@ -66,7 +66,7 @@ namespace Overworld {
             if (other.tag == "Player" && colliderTransition == true)
             {
                 player = FindObjectOfType<playerController>();
-                player.freezePlayer();
+                if (player != null) { player.freezePlayer(); }
                 SetSpawnPoint();
                 changeScene();
             }
