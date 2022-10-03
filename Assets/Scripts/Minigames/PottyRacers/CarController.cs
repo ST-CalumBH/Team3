d57b1494d3d9edfd651a91a7e7d627ed854d082b
ST-CalumBH <101605642@student.swin.edu.au>
2022-10-03 17:00:07 +1100

    Updating Code

--- Assets/Scripts/Minigames/PottyRacers/CarController.cs
+++ Assets/Scripts/Minigames/PottyRacers/CarController.cs
@@ -102,4 +102,26 @@ public class CarController : MonoBehaviour
         carRigidbody2D.velocity = forwardVelocity + rightVelocity * driftFactor;
     }
 
+    float GetLateralVeloctiy()
+    {
+        return Vector2.Dot(transform.right, carRigidbody2D.velocity);
+    }
+
+    public bool IsTireScreeching(out float lateralVelocity, out bool isBraking)
+    {
+        lateralVelocity = GetLateralVeloctiy();
+        {
+            isBraking = false;
+            if( accelInput <0 && velocityVsUp > 0)
+            {
+                isBraking = true;
+                return true;
+            }
+
+            if (Mathf.Abs(GetLateralVeloctiy()) > 4.0f)
+                return true;
+
+            return false;
+        }
+    }
 }
