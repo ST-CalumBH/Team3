331409e8739b619f8329f2725c35881cbd299a57
Jonathon S <102579324@student.swin.edu.au>
2022-10-05 02:29:13 +1100

    Fixed double popu in boss combat

---
+++ Assets/Scripts/Minigames/emptyMinigame.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class emptyMinigame : Minigame
+{
+    bool end;
+    void Start()
+    {
+        Debug.Log("empty Start");
+        end = true;
+    }
+
+    IEnumerator endGame()
+    {
+        StartCoroutine(EndMinigame(0f, true));
+        yield return null;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(end)
+        {
+            end = false;
+            StartCoroutine(endGame());
+        }
+    }
+}
